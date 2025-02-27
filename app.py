beeb0289772d946dc48cc7d5a4aa8a540f5775af
Maitry <maitrysharma5@gmail.com>
2025-02-27 22:34:43 +0530

    Creating the initial roles and the users-basic_set_up_v1

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from application.models import User,Role
 from application.config import LocalDevelopmentConfig
 
 from flask_security import Security,SQLAlchemyUserDatastore
+from flask_security import hash_password
 
 def create_app():
     app=Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI']
     app.config.from_object(LocalDevelopmentConfig)
     db.init_app(app) #to connect the application with the database
     
@@ -21,6 +21,34 @@ def create_app():
     return app
 
 app=create_app()
+
+#when we do database operation within app.py, we do it under app.cpntext
+#For any operation that happens to the database, we create context
+with app.app_context():
+    db.create_all()
+    
+    app.security.datastore.find_or_create_role(name="admin",description="superuser")
+    app.security.datastore.find_or_create_role(name="customer",description="Those who book the services")
+    app.security.datastore.find_or_create_role(name="professional",description="Those who provide the services")
+    db.session.commit()
+    
+    
+    if not app.security.datastore.find_user(email="admin@gmail.com"):
+        app.security.datastore.create_user(email="admin@gmail.com",
+                                           username="admin",
+                                           password=hash_password("admin"),
+                                           roles=['admin'])
+    
+    
+    if not app.security.datastore.find_user(email="customer1@gmail.com"):
+        app.security.datastore.create_user(email="customer1@gmail.com",
+                                           username="customer1",
+                                           password=hash_password("customer1"),
+                                           roles=['customer'])  
+    
+    db.session.commit()
+    
+    
 
 if __name__=="__main__":
     app.run()
--- application/models.py
+++ application/models.py
@@ -5,6 +5,7 @@ class User(db.Model,UserMixin):
     #required attribute of flask security (authenticates,authorizes and provides token)
     id=db.Column(db.Integer,primary_key=True)
     email=db.Column(db.String,unique=True,nullable=False)
+    username=db.Column(db.String,unique=True,nullable=False)
     password=db.Column(db.String,nullable=False)    
     fs_uniquifier=db.Column(db.String,unique=True,nullable=False)   #required by flask security, helps in token generation, 
     active=db.Column(db.Boolean,nullable=False)
@@ -19,7 +20,7 @@ class Role(db.Model,RoleMixin):
     description=db.Column(db.String)
     
 # many-to-many(for many-to-many relationship we create an association table)
-class UsersRoles(db.Model):
+class UserRoles(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
     role_id=db.Column(db.Integer,db.ForeignKey('role.id'))
