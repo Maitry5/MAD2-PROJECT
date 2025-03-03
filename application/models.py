fdabdd083657f869e9e55df660c21b85824046ec
Maitry <maitrysharma5@gmail.com>
2025-03-04 01:16:36 +0530

    Customer Registeration set up in the backend

--- application/models.py
+++ application/models.py
@@ -2,14 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_security import UserMixin, RoleMixin
 from sqlalchemy import CheckConstraint, ForeignKey
 from datetime import datetime
-
-db = SQLAlchemy()
-
-# Association table for many-to-many relationship between Users and Roles
-class UserRoles(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id', ondelete='CASCADE'))
+from .database import db
 
 
 class Role(db.Model, RoleMixin):
@@ -44,6 +37,14 @@ class User(db.Model, UserMixin):
     service_requests_as_professional = db.relationship('ServiceRequest', foreign_keys='ServiceRequest.professional_id', backref='professional', lazy=True)
 
 
+# Association table for many-to-many relationship between Users and Roles
+class UserRoles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id', ondelete='CASCADE'))
+
+
+
 class Service(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
@@ -58,7 +59,7 @@ class Service(db.Model):
 
 class ServiceRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    service_id = db.Column(db.Integer, db.ForeignKey('service.id', ondelete='CASCADE'), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id', ondelete='RESTRICT'), nullable=False)
     customer_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     professional_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='SET NULL'), nullable=True)
 
--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,8 @@
-from flask import current_app as app,jsonify
-from flask_security import auth_required,roles_required,current_user
+from flask import current_app as app,jsonify,request
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import auth_required,roles_required,current_user,hash_password
+from .database import db
+
 
 @app.route('/api/admin')
 @auth_required('token') #Authentication
@@ -20,3 +23,24 @@ def user_home():
         "email":user.email,
         "password": user.password    
     })
+    
+    
+@app.route('/api/customer/register',methods=['POST'])
+def create_customer():
+    credentials=request.get_json()
+    
+    if not app.security.datastore.find_user(email=credentials["email"]):
+        app.security.datastore.create_user(email=credentials["email"],
+                                           username=credentials["username"],
+                                           password=hash_password(credentials["password"]),
+                                           roles=["customer"],
+                                           city=credentials["city"])  
+    
+        db.session.commit()
+        return jsonify({
+            "message":"User created successfully"
+        }), 201
+    return jsonify({
+            "message":"User already exists"
+        }), 400
+    
